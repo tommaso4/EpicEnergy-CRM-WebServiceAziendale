a203a7ad5110288b11e07c92dc4f13ed869007b1
Tommaso Cantarini <tommasocantarini4@gmail.com>
2024-02-26 09:58:42 +0100

    m

---
+++ src/main/java/com/app/EpicEnergyCRM/EpicEnergyCrmApplication.java
@@ -0,0 +1,13 @@
+package com.app.EpicEnergyCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/EpicEnergyCRM/EpicEnergyCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.EpicEnergyCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
